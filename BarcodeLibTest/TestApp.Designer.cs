1d08e5736d0e8c05843193e5b75a292903d9701c
bradbarnhill@hotmail.com <bradbarnhill@hotmail.com>
2013-06-22 13:19:44 -0500

    version bumped to 1.0.0.16 and updated release notes

--- BarcodeLibTest/TestApp.Designer.cs
+++ BarcodeLibTest/TestApp.Designer.cs
@@ -82,7 +82,7 @@ namespace BarcodeLibTest
             this.barcode.Dock = System.Windows.Forms.DockStyle.Fill;
             this.barcode.Location = new System.Drawing.Point(3, 16);
             this.barcode.Name = "barcode";
-            this.barcode.Size = new System.Drawing.Size(406, 324);
+            this.barcode.Size = new System.Drawing.Size(395, 317);
             this.barcode.TabIndex = 13;
             this.barcode.TabStop = false;
             // 
@@ -119,9 +119,9 @@ namespace BarcodeLibTest
             this.tsslEncodedType,
             this.tslblLibraryVersion,
             this.tslblCredits});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 343);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 336);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(634, 24);
+            this.statusStrip1.Size = new System.Drawing.Size(623, 24);
             this.statusStrip1.SizingGrip = false;
             this.statusStrip1.TabIndex = 30;
             this.statusStrip1.Text = "statusStrip1";
@@ -136,7 +136,7 @@ namespace BarcodeLibTest
             // 
             this.tslblLibraryVersion.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
             this.tslblLibraryVersion.Name = "tslblLibraryVersion";
-            this.tslblLibraryVersion.Size = new System.Drawing.Size(480, 19);
+            this.tslblLibraryVersion.Size = new System.Drawing.Size(469, 19);
             this.tslblLibraryVersion.Spring = true;
             this.tslblLibraryVersion.Text = "LibVersion";
             // 
@@ -188,7 +188,7 @@ namespace BarcodeLibTest
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(218, 343);
+            this.groupBox1.Size = new System.Drawing.Size(218, 336);
             this.groupBox1.TabIndex = 35;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "User Information";
@@ -452,7 +452,7 @@ namespace BarcodeLibTest
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Location = new System.Drawing.Point(0, 0);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(412, 343);
+            this.groupBox2.Size = new System.Drawing.Size(401, 336);
             this.groupBox2.TabIndex = 36;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Barcode Image";
@@ -472,7 +472,7 @@ namespace BarcodeLibTest
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
-            this.splitContainer1.Size = new System.Drawing.Size(634, 343);
+            this.splitContainer1.Size = new System.Drawing.Size(623, 336);
             this.splitContainer1.SplitterDistance = 218;
             this.splitContainer1.TabIndex = 37;
             this.splitContainer1.SplitterMoved += new System.Windows.Forms.SplitterEventHandler(this.splitContainer1_SplitterMoved);
@@ -485,7 +485,7 @@ namespace BarcodeLibTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(634, 367);
+            this.ClientSize = new System.Drawing.Size(623, 360);
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.statusStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
