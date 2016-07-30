886eb9a865ba62a4014a272ddca83d58eaeb7260
Brad Barnhill <bradbarnhill@hotmail.com>
2016-07-29 19:42:13 -0500

    fix bug in index of upc-e encoding, bump version for release

--- BarcodeLib/Symbologies/UPCE.cs
+++ BarcodeLib/Symbologies/UPCE.cs
@@ -12,7 +12,6 @@ namespace BarcodeLib.Symbologies
     {
         private string[] EAN_CodeA = { "0001101", "0011001", "0010011", "0111101", "0100011", "0110001", "0101111", "0111011", "0110111", "0001011" };
         private string[] EAN_CodeB = { "0100111", "0110011", "0011011", "0100001", "0011101", "0111001", "0000101", "0010001", "0001001", "0010111" };
-        private string[] EAN_CodeC = { "1110010", "1100110", "1101100", "1000010", "1011100", "1001110", "1010000", "1000100", "1001000", "1110100" };
         private string[] EAN_Pattern = { "aaaaaa", "aababb", "aabbab", "aabbba", "abaabb", "abbaab", "abbbaa", "ababab", "ababba", "abbaba" };
         private string[] UPCE_Code_0 = { "bbbaaa", "bbabaa", "bbaaba", "bbaaab", "babbaa", "baabba", "baaabb", "bababa", "babaab", "baabab" };
         private string[] UPCE_Code_1 = { "aaabbb", "aababb", "aabbab", "aabbba", "abaabb", "abbaab", "abbbaa", "ababab", "ababba", "abbaba" };
@@ -94,7 +93,7 @@ namespace BarcodeLib.Symbologies
             //encode the data
             string result = "101";
 
-            int pos = 1;
+            int pos = 0;
             foreach (char c in pattern)
             {
                 int i = Int32.Parse(Raw_Data[pos++].ToString());
