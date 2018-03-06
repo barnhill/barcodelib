f1a8d80047dd2a0ba7d7b37d7f2d962e43a13c8e
Brad Barnhill <bradfordbarnhill@gmail.com>
2018-03-05 20:56:58 -0600

    release 1.1.0

--- BarcodeLib/Properties/AssemblyInfo.cs
+++ BarcodeLib/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Security;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.23")]
-[assembly: AssemblyFileVersion("1.0.0.23")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
